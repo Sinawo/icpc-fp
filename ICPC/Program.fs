51e0fa42bf00034f4a46673a6ccd7bb95acd8cb7
Sinawo Mngxuma <g16M4204@ict.ru.ac.za>
2019-04-16 15:02:44 +0200

    edit setInput Method

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -6,7 +6,6 @@ let Findwords (input : string) (index : int)=
    
       
       let rec search statingIndex i =
-        let firstCharForNext = input.[statingIndex]   /////
         match input.[i] <> ' ' with
         | true -> search statingIndex (i+1)
         |  _ ->  
@@ -14,24 +13,11 @@ let Findwords (input : string) (index : int)=
                  foundword
       search index (index)
               
- 
+let addcomma substring : string =
+        
+        let substring = substring + "," in
+         substring
 
-
-
-let setInput (input : string) (mystring : string)(*contains comma*) (index : int) = //failwith "j"
-                                
-                             let word =  in
-                                word
-                               let rec myf index =
-                                let found = Findwords input index in
-                                 match word.CompareTo(found) with
-                                 | 1 -> 
-                                       let input = input + "," + 
-                                 
-                                 | 2 -> myf index+1
-
-
-                   
 
  (*this method returns the word which contains a comma or "NotFound" if there's no comma*)
 let IsComma (word :string) =
@@ -53,6 +39,27 @@ let GetNextIndex (input : string) (index : int)=
 
 let SucceedingComma (input : string)  (word : string) = failwith "kkk"
 
+let setInput (input : string) (mystring : string)(*contains comma*) (indexToStartAt : int) (indexOfn : int) = //failwith "j"
+                                
+                            
+                            let mystring = mystring.[..mystring.Length-2] // since it contains comma
+                            let rec myf index =
+                                 
+                                let foundword = Findwords input index
+                                let indexToStartAt = GetNextIndex input index
+                                match indexToStartAt < input.Length with 
+                                |true ->    match mystring = foundword with
+                                             | true ->   let substring = input.[..indexOfn-1]
+                                                         let LengthOfSub= substring.Length + foundword.Length
+                                                         let rec fill i editedSubstring =
+                                                               match i <> LengthOfSub with
+                                                               | true -> fill (i+1) ( editedSubstring + (string)input.[i]) 
+                                                               | false ->  editedSubstring
+                                                         fill index substring
+                                             | _ ->  myf (indexToStartAt)
+                                |false -> input
+                            myf indexToStartAt /// or  index to start at
+
 
                          
 let commaSprinkler (input : string)  =
@@ -60,17 +67,17 @@ let commaSprinkler (input : string)  =
                        let indexToStartAt = 0;
 
 
-                       let rec myf string index =
+                       let rec myf  index =
 
                        //recursion here-----------------------------------------------------------
                           let mystring = Findwords (input) (index)   // 1. find word in sentnce
                          // let isComma = IsComma mystring                  
                           let indexToStartAt = GetNextIndex input index in // 3. get the next index where we should start finding these words
                                    match mystring.EndsWith(',') && mystring.[mystring.Length-1] <> ('\t')  with       // 2. check if 1. has comma
-                                   | false -> myf input indexToStartAt
-                                   | true ->  let nmystring = setInput input mystring indexToStartAt 
-                                              mystring
-                       myf input 0
+                                   | false -> myf indexToStartAt
+                                   | true ->  let nmystring = setInput input mystring indexToStartAt index
+                                              nmystring
+                       myf 0
                      
 
 let rivers input =
